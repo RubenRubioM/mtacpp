e54f376e4c926bfcfa41b6f1e697561bef522031
rubenrubiom <rubensipala@gmail.com>
2024-06-26 10:50:22 +0200

    Added documentation and using declarations

--- include/mta.h
+++ include/mta.h
@@ -6,12 +6,46 @@
 
 namespace mta
 {
-    template <typename Duration = std::chrono::duration<double, std::nano>,
-              typename Clock = std::chrono::system_clock>
+    static constexpr uint64_t secondsRatio = 1;
+    static constexpr uint64_t minutesRatio = secondsRatio * 60;
+    static constexpr uint64_t hoursRatio = minutesRatio * 60;
+    static constexpr uint64_t daysRatio = hoursRatio * 24;
+    static constexpr uint64_t weeksRatio = daysRatio * 7;
+    static constexpr uint64_t monthsRatio = 2629746;
+    static constexpr uint64_t yearsRatio = 31556952;
+
+    using DecimalNanoseconds = std::chrono::duration<double, std::nano>;
+    using DecimalMicroseconds = std::chrono::duration<double, std::micro>;
+    using DecimalMilliseconds = std::chrono::duration<double, std::milli>;
+    using DecimalCentiseconds = std::chrono::duration<double, std::centi>;
+    using DecimalDeciseconds = std::chrono::duration<double, std::deci>;
+    using DecimalSeconds = std::chrono::duration<double, std::ratio<secondsRatio>>;
+    using DecimalMinutes = std::chrono::duration<double, std::ratio<minutesRatio>>;
+    using DecimalHours = std::chrono::duration<double, std::ratio<hoursRatio>>;
+    using DecimalDays = std::chrono::duration<double, std::ratio<daysRatio>>;
+    using DecimalWeeks = std::chrono::duration<double, std::ratio<weeksRatio>>;
+    using DecimalMonths = std::chrono::duration<double, std::ratio<monthsRatio>>;
+    using DecimalYears = std::chrono::duration<double, std::ratio<yearsRatio>>;
+
+    using IntegerNanoseconds = std::chrono::duration<uint64_t, std::nano>;
+    using IntegerMicroseconds = std::chrono::duration<uint64_t, std::micro>;
+    using IntegerMilliseconds = std::chrono::duration<uint64_t, std::milli>;
+    using IntegerCentiseconds = std::chrono::duration<uint64_t, std::centi>;
+    using IntegerDeciseconds = std::chrono::duration<uint64_t, std::deci>;
+    using IntegerSeconds = std::chrono::duration<uint64_t, std::ratio<secondsRatio>>;
+    using IntegerMinutes = std::chrono::duration<uint64_t, std::ratio<minutesRatio>>;
+    using IntegerHours = std::chrono::duration<uint64_t, std::ratio<hoursRatio>>;
+    using IntegerDays = std::chrono::duration<uint64_t, std::ratio<daysRatio>>;
+    using IntegerWeeks = std::chrono::duration<uint64_t, std::ratio<weeksRatio>>;
+    using IntegerMonths = std::chrono::duration<uint64_t, std::ratio<monthsRatio>>;
+    using IntegerYears = std::chrono::duration<uint64_t, std::ratio<yearsRatio>>;
+
+    template <typename Clock = std::chrono::high_resolution_clock>
     class Timer
     {
     public:
-        explicit Timer() noexcept = default;
+
+        constexpr explicit Timer() noexcept = default;
         ~Timer() noexcept = default;
 
         void start() noexcept
@@ -19,20 +53,32 @@ namespace mta
             init = Clock::now();
         }
 
-        template <typename ConvertibleDuration = Duration>
+        /// @brief Returns the elapsed time since Timer::start was called
+        /// @tparam ConvertibleDuration std::chrono::duration to convert to this unit.
+        /// @note Encourage to use the `using` defined in the namespace `mta` that starts from Decimal and Integer. 
+        ///       You can also provide your own conversion directly from chrono, e.g: std::chrono::milliseconds.
+        ///       The default value is `mta::DecimalNanoseconds`, nanoseconds in floating point representation.
+        /// @return The value of T between Timer::start and now in the underlying type used in ConvertibleDuration.
+        template <typename ConvertibleDuration = DecimalNanoseconds>
         [[nodiscard]] ConvertibleDuration::rep elapsed() const noexcept
         {
             return elapsedAsDuration<ConvertibleDuration>().count();
         }
 
-        template <typename ConvertibleDuration = Duration>
+        /// @brief Returns the elapsed time since Timer::start was called as an std::chrono::duration
+        /// @tparam ConvertibleDuration std::chrono::duration to convert to this unit.
+        /// @note Encourage to use the `using` defined in the namespace `mta` that starts from Decimal and Integer. 
+        ///       You can also provide your own conversion directly from chrono, e.g: std::chrono::milliseconds.
+        ///       The default value is `mta::DecimalNanoseconds`, nanoseconds in floating point representation.
+        /// @return The value of T between Timer::start and now.
+        template <typename ConvertibleDuration = DecimalNanoseconds>
         [[nodiscard]] ConvertibleDuration elapsedAsDuration() const noexcept
         {
             return std::chrono::duration_cast<ConvertibleDuration>(Clock::now() - init);
         }
 
     private:
-        std::chrono::time_point<Clock> init{};
+        std::chrono::time_point<Clock> init{}; /// Initial time_point. Setted in Timer::start.
     }; // class Timer
 
 } // namespace mta
--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,14 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
-#include <format>
-#include <printf.h>
+
+using namespace std::chrono_literals;
 
 int main()
 {
-    mta::Timer<std::chrono::milliseconds> timer;
+    mta::Timer timer;
     timer.start();
-    // std::chrono::time_point tp = std::chrono::high_resolution_clock::now();
-
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-
-    // std::chrono::time_point tp2 = std::chrono::high_resolution_clock::now();
-    // std::chrono::duration d = std::chrono::duration<double, std::ratio<60>>(tp2 - tp);
-    // std::cout << "Duration: " << d << '\n';
-    std::cout << "As Duration: " << timer.elapsedAsDuration<std::chrono::duration<double, std::milli>>() << '\n';
-    std::cout << "As count(): " << timer.elapsed<std::chrono::duration<int, std::milli>>() << '\n';
+    std::this_thread::sleep_for(1s);
+    std::cout << "As Duration: " << timer.elapsedAsDuration<mta::DecimalSeconds>() << '\n';
+    std::cout << "As count(): " << timer.elapsed<mta::IntegerSeconds>() << '\n';
 }
